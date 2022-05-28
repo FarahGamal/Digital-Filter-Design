b44631291010db453ab5ff295e5641a2874a8e8b
Hoda Azzam <100151668+HodaAzzam@users.noreply.github.com>
2022-05-28 16:56:50 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,30 +1,31 @@
-from matplotlib.pyplot import show
+import fileinput
 import numpy as np
-from bokeh.plotting import figure, Column,Row
-from bokeh.models import PointDrawTool, ColumnDataSource,Button,Div
-from bokeh.models.widgets import RadioButtonGroup
 from bokeh.layouts import *
 from bokeh.io import curdoc
+from ctypes import alignment
+from turtle import color, width
 from bokeh.events import DoubleTap
+from matplotlib.pyplot import show
 from scipy.signal import zpk2tf, freqz
-import numpy as np
 from bokeh.models import Dropdown,RadioGroup
+from bokeh.plotting import figure, Column,Row
+from bokeh.models import PointDrawTool, ColumnDataSource,Button,Div
+from bokeh.models.widgets import RadioButtonGroup, FileInput, TextInput
 
-#? To run the code write (python -m bokeh serve --show test.py) in terminal <3 
+
+#? To run the code write python -m bokeh serve --show test.py in terminal <3 
  
 ## Plots and Graphs 
 unitCirclePlot = figure(x_range=(-2,2), y_range=(-2,2), tools=[],
            title='zPolar',plot_width=500, plot_height=500)
 allPassUnitCirclePlot = figure(x_range=(-2,2), y_range=(-2,2), tools=[],
-           title='Filter Designer',plot_width=500, plot_height=500)
+           title='Zero-Ploe Combination Of Selected All-pass Filter',plot_width=500, plot_height=500)
 phasePlot=figure(x_range=(0,3.14), y_range=(-3.14,3.14), tools=['pan,box_zoom'],
-           title='Phase',plot_width=500, plot_height=500)
+           title='Phase Response',plot_width=500, plot_height=500)
 magnitudePlot=figure(x_range=(0,3.14), y_range=(0,3), tools=['pan,box_zoom'],
-           title='Magnitude',plot_width=500, plot_height=500)           
+           title='Magnitude Response',plot_width=500, plot_height=500)           
 phaseResponseOfFilter=figure(x_range=(0,3.14), y_range=(-3.14,3.14), tools=['pan,box_zoom'],
-title='Phase Response',plot_width=500, plot_height=500)
-magResponseOfFilter=figure(x_range=(0,3.14), y_range=(-3.14,3.14), tools=['pan,box_zoom'],
-title='Magnitude Response',plot_width=500, plot_height=500)
+title='Phase Response of selected All-pass Filter',plot_width=500, plot_height=500)
 originalSignal=figure(x_range=(0,10), y_range=(-10,10), tools=['pan,box_zoom'],
 title='Original Signal',plot_width=500, plot_height=500)
 filteredSignal=figure(x_range=(0,10), y_range=(-10,10), tools=['pan,box_zoom'],
@@ -50,20 +51,25 @@ filterP=ColumnDataSource({
     'w':[], 'p':[]
 })
 
-##Buttons
+##Buttons and controls
 poleOrZeroSelection = RadioButtonGroup(labels=['Zero', 'Pole'], active=0)
 conjugateSelection = RadioButtonGroup(labels=['No conjugate', 'Conjugate'], active=0)
 resetAll = Button(label='Reset all')
 clearZeros= Button(label='Clear Zeros')
 clearPoles= Button(label='Clear Poles')
-filtersMenu = [("Filter 1", "0"), ("Filter 2", "1"), ("Filter 3", "2")]
+filtersLibrary = [("Filter 1", "0"), ("Filter 2", "1"), ("Filter 3", "2")]
+appliedAllPassFilters=[(" Designed Filter 1","0" )] 
 applyFilterButton = Button(label='Apply')
-resetFilterButton = Button(label='Reset filter')
+removeFilterButton = Button(label='Remove Selected All-pass Filter')
 addToFiltersList = Button(label='Add to filters list')
-filtersDropdownMenu = Dropdown(label="Filters picker", button_type="warning", menu=filtersMenu)
-openFile= Button(label='Open Signal')
+filtersDropdownMenu = Dropdown(label="All-pass Filters Library", button_type="warning", menu=filtersLibrary)
+appliedFiltersDropdownMenu = Dropdown(label="Applied All-pass Filters to Designed Digital Filter", button_type="warning", menu=appliedAllPassFilters)
+realInputOfFilter= TextInput(title='')
+imgInputOfFilter= TextInput(title='')
+openFile= FileInput(accept= '.csv', width=500)
 applyToSignal= Button(label='Apply Filter on Signal')
-##Unit Circles
+
+##Unit Circles Plotting
 unitCirclePlot.circle(0,0,radius=1,fill_color=None,line_color='red')
 allPassUnitCirclePlot.circle(0,0,radius=1,fill_color=None,line_color='red')
 
@@ -84,6 +90,6 @@ allPassUnitCirclePlot.toolbar.active_tap = draw_tool2
 
 welcomeMsg= Div(text='<h2>Welcome to Our Digital Filter Designer! </h2>', align= 'center')
 
-layout=Column(welcomeMsg,Row(poleOrZeroSelection,conjugateSelection,clearPoles,clearZeros,resetAll),Row(unitCirclePlot,phasePlot,magnitudePlot),Row(filtersDropdownMenu,applyFilterButton,resetFilterButton,addToFiltersList),Row(allPassUnitCirclePlot,phaseResponseOfFilter,magResponseOfFilter),Row(openFile,applyToSignal),Row(originalSignal,filteredSignal))
+layout=Column(welcomeMsg,Row(poleOrZeroSelection,conjugateSelection,clearPoles,clearZeros,resetAll),Row(unitCirclePlot,phasePlot,magnitudePlot),Row(filtersDropdownMenu,addToFiltersList,removeFilterButton,appliedFiltersDropdownMenu ),Row(allPassUnitCirclePlot,phaseResponseOfFilter,Column(Row(Div(text='a ='),realInputOfFilter, Div(text='+ j'), imgInputOfFilter),applyFilterButton)),Row(openFile,applyToSignal),Row(originalSignal,filteredSignal))
 curdoc().add_root(layout)
 
